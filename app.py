9e4af1ae748850f13bec55c6d3d919de79d7db5a
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-30 23:01:23 -0400

    Created the initial endpoints.

--- app.py
+++ app.py
@@ -2,4 +2,30 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+
+@app.route('/store')
+def get_stores():
+    pass
+
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item(name):
+    pass
+
+
+@app.route('/store/<string:name>/item')
+def get_item(name):
+    pass
+
+
 app.run(port=5000)
